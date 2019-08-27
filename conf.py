5fdf781b4a47e37997019390e6613a51504d43b1
Walter Arrighetti <walter-arrighetti@users.noreply.github.com>
2019-08-27 12:08:03 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@ master_doc = 'index'                      # The master toctree document.
 #### Options for LaTeX output
 latex_elements = { 'papersize':"a4paper" }
 
-latex_logo = "media/images/AgID-logo.png"  # The name of an image file to place at the top of the title page.
+latex_logo = "media/images/AgID_logo.png"  # The name of an image file to place at the top of the title page.
 latex_show_pagerefs = False                # If true, show page references after internal links.
 latex_show_urls = False                    # If true, show URL addresses after external links.
 
