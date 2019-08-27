9ca6a4d5999bf02d68eb6d8c0ee8cd9a79b26758
Walter Arrighetti <walter-arrighetti@users.noreply.github.com>
2019-08-27 11:38:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Regole Tecniche circa la sottoscrizione elettronica di documenti mediante SPID ex art. 20 del CAD
+## Linee Guida AgID ex art. 71 del CAD
+
+L’articolo 20 del CAD dispone il soddisfacimento del requisito della forma scritta e l’efficacia prevista dall’articolo 2702 del Codice Civile del documento informatico formato previa identificazione informatica del suo autore, *attraverso un processo avente i requisiti fissati dall’AgID ai sensi dell’articolo 71 con modalità tali da garantire la sicurezza, integrità e immodificabilità del documento e, in maniera manifesta e inequivoca, la sua riconducibilità all’autore*.
+Le presenti Linee guida regolano le modalità atte a garantire la sicurezza, integrità e immodificabilità del documento e, in maniera manifesta e inequivoca, la sua riconducibilità all’autore.
+
+
+## Link al documento e consultazione
+
+Link al documento Docs Italia: https://lg-spid-sottoscrizione-documenti-ex-art20-cad.readthedocs.io
+
+Link per commenti Forum Italia: https://forum.italia.it/c/documenti-in-consultazione/lg-spid-sottoscrizione-documenti-ex-art20-cad
+
+La consultazione sarà attiva dal 15 settembre 2019 al 15 ottobre 2019.
---
+++ conf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# The master toctree document.	
+master_doc = 'index'
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+'papersize': 'a4paper',
+}
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+latex_logo = "media/agid-logo.png"
+
+# If true, show page references after internal links.
+latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+latex_appendices = [
+    "attachments/allegato-b-guida-alla-pubblicazione-open-source-di-software-realizzato-per-la-pa",
+    "attachments/allegato-c-guida-alla-manutenzione-di-software-open-source",
+    "attachments/allegato-d-guida-alle-licenze-open-source",
+    "attachments/allegato-e-guida-alla-modifica-di-software-open-source-preso-a-riuso-o-di-terzi",
+    "attachments/allegato-e-guida-alla-modifica-di-software-open-source-preso-a-riuso-o-di-terzi",
+    "attachments/allegato-f-tabella-sinottica-degli-elementi-necessari-al-percorso-decisionale",
+]
